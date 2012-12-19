9639b05299ec0ac677b22f59d6c1e9d17beada8b
martinkosk <kosk.martin@gmail.com>
2012-12-19 22:02:14 +0200

    Added GuardInBorderStationDao interface and implementation

---
+++ src/ee/itcollege/borderproject/dao/impl/GuardInBorderStationDaoJpa.java
@@ -0,0 +1,30 @@
+package ee.itcollege.borderproject.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import ee.itcollege.borderproject.common.CrudDaoJpa;
+import ee.itcollege.borderproject.dao.GuardInBorderStationDao;
+import ee.itcollege.borderproject.model.GuardInBorderStation;
+
+@Repository
+public class GuardInBorderStationDaoJpa extends CrudDaoJpa<GuardInBorderStation> implements GuardInBorderStationDao{
+
+	@Override
+	public List<GuardInBorderStation> getAll() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		try {
+			TypedQuery<GuardInBorderStation> query = entityManager.createNamedQuery("GuardInBorderStation.findAll", GuardInBorderStation.class);
+			return query.getResultList();
+		}
+		finally {
+			entityManager.close();
+		}
+	}
+
+}
